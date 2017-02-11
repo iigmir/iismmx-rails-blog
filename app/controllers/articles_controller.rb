aa5c2d12f5cdd11202cb04dc0dd7436881232c07
iigmir <roc120j@gmail.com>
2017-02-11 16:58:00 +0800

    Add final methods... Ref: http://stackoverflow.com/questions/26895951/undefined-local-variable-or-method-destroy-posts-path

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
+
   def new
     @article = Article.new
   end
@@ -8,6 +10,25 @@ class ArticlesController < ApplicationController
     Article.create( art_params )
     redirect_to articles_path
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    Article.update( art_params )
+    redirect_to articles_path
+    #  What's different between:
+    #  @job = Job.find(params[:id])
+    #  @job.update_attributes(job_params)
+    #  redirect_to jobs_path
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def art_params
