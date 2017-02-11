80b1df0222d27919e23c9f240b029a2b195383e0
iigmir <roc120j@gmail.com>
2017-02-11 16:17:11 +0800

    sent action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,13 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def create
+    Article.create( art_params )
+    redirect_to articles_path
+  end
+  private
+
+  def art_params
+    params.require(:article).permit(:title, :context)
+  end
 end
