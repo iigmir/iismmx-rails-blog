0d108737da96c8f945291a7296c75c51273aaea8
iigmir <roc120j@gmail.com>
2017-03-26 10:52:15 +0800

    build whole link struchar

--- app/controllers/mylinks_controller.rb
+++ app/controllers/mylinks_controller.rb
@@ -1,5 +1,39 @@
 class MylinksController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
+  # C
+  def new
+    @link = Mylink.new
+  end
+  def create
+    Article.create( link_params )
+    redirect_to mylinks_path
+  end
+
+  # R
   def index
+    @links = Mylink.order(id: :desc)
+  end
+
+  # U
+  def edit
+    @link = Mylink.find(params[:id])
+  end
+  def update
+    @link = Mylink.find(params[:id])
+    @link.update_attributes(link_params)
+    redirect_to mylinks_path
+  end
+
+  # D
+  def destory
+    @link = Mylink.find(params[:id])
+    @line.destroy
+    redirect_to mylinks_path
+  end
+
+  # params
+  private
+  def link_params
+    params.require(:article).permit(:site, :name, :intro, :group))
   end
 end
