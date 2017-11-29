c22494f66e14c0448678599a2ce393b62513f688
iigmir <roc120j@gmail.com>
2017-11-29 22:43:53 +0800

    DRY whole SNS Link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+    def sns_link( sns_type )
+        def sns_type_switch( sts_input )
+            case sts_input
+            when "firefox"
+                return "https://developer.mozilla.org/zh-TW/profiles/"
+            when "github"
+                return "https://www.github.com/"
+            when "codepen"
+                return "https://codepen.io/"
+            else
+                return root_path
+            end
+        end
+        account_name = "iigmir"
+        website = sns_type_switch( sns_type ) + account_name
+
+        link_to website, target: "_blank" do
+            fa_icon sns_type
+        end
+    end
+
+    def login_and_out()
+        if user_signed_in?
+            "in"
+        else
+            "out"
+        end
+    end
 end
