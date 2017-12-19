c2bf20533bd1303413b2ec03f4687e8d1c5a5a0d
iigmir <roc120j@gmail.com>
2017-12-19 23:06:06 +0800

    Fix rels part3

--- db/migrate/20171212175519_create_categories.rb
+++ db/migrate/20171212175519_create_categories.rb
@@ -1,6 +1,6 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
     def change
-        create_table :categories do |t|
+        create_table :article_tags do |t|
             t.references :article, foreign_key: true
             t.references :article_tags, foreign_key: true
             t.timestamps
