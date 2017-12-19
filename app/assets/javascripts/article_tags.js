52e68912992bf513db07fc185f551566ca53875d
iigmir <roc120j@gmail.com>
2017-12-19 19:41:20 +0800

    Styling tag manage, and JS render.

--- app/assets/javascripts/article_tags.js
+++ app/assets/javascripts/article_tags.js
@@ -1,5 +1,7 @@
 $(document).ready(function()
 {
+    
+
     $.ajax
     ({
         url: "/article_tags.json",
@@ -15,6 +17,7 @@ $(document).ready(function()
     function init_render( ir_input )
     {
         var btn_curr = "";
+        
         ir_input.forEach(function( rgo_input )
         {
             var btn_tmpdata, btn_tmpview, btn_html_dataset;
@@ -22,22 +25,29 @@ $(document).ready(function()
                 idnum: "/article_tags/" + rgo_input.id,
                 value: rgo_input.tag_name
             };
-            btn_html_dataset = `data-component="modal" data-target="#articles_modal" data-render-api="${btn_tmpdata.idnum}" `;
-            btn_tmpview = `<button class='button' ${ btn_html_dataset }> ${btn_tmpdata.value} </button>`;
+            // btn_html_dataset = `data-component="modal" data-target="#articles_modal" data-render-api="${btn_tmpdata.idnum}" `;
+            btn_tmpview = `<button class='button'
+                data-component="modal" data-target="#articles_modal"
+                data-render-api="${btn_tmpdata.idnum}"
+            > ${btn_tmpdata.value} </button>`;
             return btn_curr += btn_tmpview;
         });
         
         $("#render_view").html(btn_curr);
 
-        $('.button').click(function (event)
+        $('#render_view .button').click(function (event)
         {   // What the fucking AJAX...Fucking work in fucking AJAX?
             var clicked_value = event.target.innerHTML;
+            var render_api = event.target.dataset.renderApi;
             var help_text = "以下文章內容與" + clicked_value + "有關：";
-            $( "#articles_modal .clicked_value" ).text( clicked_value );
+            
             $( "#articles_modal .help_text" ).text( help_text );
+            $( "#articles_modal .clicked_value" ).text( clicked_value );
+            $( '#articles_modal a.button[data-method="delete"]' ).attr( "href" , render_api );
+
             $.ajax
             ({
-                url: event.target.dataset.renderApi,
+                url: render_api,
                 dataType: "json",
                 error: function(jqXHR, textStatus, errorThrown)
                 {
@@ -47,7 +57,7 @@ $(document).ready(function()
                 success: function(data) { catagroy_render( data );return; }
             });
         });
-        
+
         return;
     }
 
