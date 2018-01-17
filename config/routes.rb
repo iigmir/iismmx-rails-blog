b7c49dec5dfdbb37de078edd3733337df696cbd7
iigmir <roc120j@gmail.com>
2018-01-18 00:55:43 +0800

    Change error pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     resources :categories
     resources :category_relations    
     root "articles#index"
-    get '*path' => redirect('/')
+    #get '*path' => redirect('/')
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
